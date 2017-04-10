d9a86928984c4cd16073eb59e36820d6dc215677
Renato Benkendorf <renato-benkendorf@hotmail.com>
2017-04-10 06:45:29 -0300

    use state components provider preserver and use initial state

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
+import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
 import App from './App';
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-);
+const render = (Container) => {
+    ReactDOM.render(
+        <AppContainer>
+            <Container {...window.INITIAL_STATE} />
+        </AppContainer>,
+        document.getElementById('root')
+    );
+};
+
+render(App);
 
 if (module.hot) {
     module.hot.accept('./App.js', () => {
         const App = require('./App.js').default;
-        ReactDOM.render(<App />, document.getElementById('root'));
+        render(App);
     });
 }
