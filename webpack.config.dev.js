637d2387085d98c37b9ea94a28c18c5b313b9110
Renato Benkendorf <renato-benkendorf@hotmail.com>
2017-04-10 18:13:50 -0300

    using provider for preserver state components on webpack loader

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -42,7 +42,7 @@ module.exports = {
             {
                 test: /\.(js|jsx)$/,
                 exclude: path.join(__dirname, "node_modules"),
-                use: "babel-loader",
+                use: ["react-hot-loader/webpack", "babel-loader"],
                 include: path.join(__dirname, "src")
             },
             {
