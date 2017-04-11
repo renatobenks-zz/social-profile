4e8ecc7a87f5898780b6734dd24082bb7157aa02
Renato Benkendorf <renato-benkendorf@hotmail.com>
2017-04-11 02:35:11 -0300

    fix path container App component

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import logo from '../public/images/logo.svg';
-import App from './components/App/App.jsx';
+import App from './components/App.jsx';
 
 const render = (Container) => {
     ReactDOM.render(
@@ -17,8 +17,8 @@ const render = (Container) => {
 render(App);
 
 if (module.hot) {
-    module.hot.accept('./App.jsx', () => {
-        const App = require('./components/App/App.jsx').default;
+    module.hot.accept('./components/App.jsx', () => {
+        const App = require('./components/App.jsx').default;
         render(App);
     });
 }
