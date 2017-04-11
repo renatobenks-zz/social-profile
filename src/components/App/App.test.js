bb56e98a4c9dd8974d91f54d12a081cc4ccdca35
Renato Benkendorf <renato-benkendorf@hotmail.com>
2017-04-10 21:17:19 -0300

    change React component to jsx extension component

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactRender from 'react-test-renderer';
 
-import App from './App';
+import App from './App.jsx';
 
 const component = ReactRender.create(
     <App logo="logo.svg" title="My title app"/>
