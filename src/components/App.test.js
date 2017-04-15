763adb8df4bce47400e6000a9efe76e31d323fcf
Renato Benkendorf <renato-benkendorf@hotmail.com>
2017-04-15 16:08:48 -0300

    fix props on app component

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactRender from 'react-test-renderer';
 
+const mockStatus = [
+    { user: 'Elton', text: 'nothing interesting' },
+    { user: 'Elton', text: 'status updated' },
+    { user: 'Elton', text: 'my status' },
+    { user: 'Vinicius', text: 'other status' }
+];
+
+const propsApp = {
+    banner: "banner.png",
+    logo: "logo.svg",
+    title: "My title app",
+    status: mockStatus
+};
+
 import App from './App.jsx';
 const component = ReactRender.create(
-    <App logo="logo.svg" title="My title app"/>
+    <App
+        {...propsApp}
+    />
 );
 
 describe('Component: App', () => {
