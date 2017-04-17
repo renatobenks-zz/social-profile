949e14616ce3c83ab95b7ed43404b05a0f5db91b
Renato Benkendorf <renato-benkendorf@hotmail.com>
2017-04-17 20:07:28 -0300

    adding more data for mock

--- src/components/components.mock.js
+++ src/components/components.mock.js
@@ -1,3 +1,15 @@
+export const mockStatus = [
+    { id: 1,user: 'Elton', text: 'nothing interesting' },
+    { id: 2,user: 'Elton', text: 'status updated' },
+    { id: 3,user: 'Elton', text: 'my status' },
+    { id: 4,user: 'Vinicius', text: 'other status' }
+];
+
+export const mockFriends = [
+    { id: 1, user: 'Elton', image: 'elton.png' },
+    { id: 2, user: 'Vinicius', image: 'vinicius.png' }
+];
+
 export const eventMock = {
     target: {value: 'my new value'},
     stopPropagation: () => true
