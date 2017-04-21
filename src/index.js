8c35bc4da185a6562685d5b2282999e4b2bed01c
Renato Benkendorf <renato-benkendorf@hotmail.com>
2017-04-21 02:14:46 -0300

    update the mock user data

--- src/index.js
+++ src/index.js
@@ -5,18 +5,62 @@ import { AppContainer } from 'react-hot-loader'
 import logo from '../public/images/logo.svg'
 import App from './components/App.jsx'
 
-const status = [
-    { id: 1, user: 'Elton', text: 'nothing interesting' },
-    { id: 2, user: 'Elton', text: 'status updated' },
-    { id: 3, user: 'Vinicius', text: 'my status' },
-    { id: 4, user: 'Vinicius', text: 'my other status' },
-    { id: 5, user: 'Vinicius', text: 'other status' }
-];
-
-const friends = [
-    { id: 1, user: 'Elton', image: '/public/images/01.avatar.jpg' },
-    { id: 2, user: 'Vinicius', image: '/public/images/02.avatar.png' }
-];
+const user = {
+    id: 1,
+    logged: true,
+    name: 'Renato Benkendorf',
+    user: 'renatão',
+    image: '/public/images/02.avatar.jpg',
+    feed: {
+        status: [
+            { id: 1, user: 'Elton', text: 'nothing interesting' },
+            { id: 2, user: 'Elton', text: 'status updated' },
+            { id: 3, user: 'Vinicius', text: 'my status' },
+            { id: 4, user: 'Vinicius', text: 'my other status' },
+            { id: 5, user: 'Vinicius', text: 'other status' },
+            { id: 6, user: 'Elton', text: 'nothing interesting' },
+            { id: 7, user: 'Elton', text: 'status updated' },
+            { id: 8, user: 'Vinicius', text: 'my status' },
+            { id: 9, user: 'Vinicius', text: 'my other status' },
+            { id: 10, user: 'Elton', text: 'nothing interesting' },
+            { id: 11, user: 'Elton', text: 'status updated' },
+            { id: 12, user: 'Vinicius', text: 'my status' },
+            { id: 13, user: 'Vinicius', text: 'my other status' }
+        ]
+    },
+    friends: [
+        {
+            id: 2,
+            user: 'Elton',
+            image: '/public/images/01.avatar.jpg',
+            favorite: true
+        },
+        {
+            id: 3,
+            user: 'Vinicius',
+            image: '/public/images/01.avatar.jpg',
+            favorite: true
+        },
+        {
+            id: 4,
+            user: 'Cozinheira',
+            image: '/public/images/01.avatar.jpg',
+            favorite: true
+        },
+        {
+            id: 5,
+            user: '',
+            image: '/public/images/01.avatar.jpg',
+            favorite: true
+        },
+        {
+            id: 6,
+            user: '',
+            image: '/public/images/01.avatar.jpg',
+            favorite: false
+        }
+    ],
+};
 
 import banner from '../public/images/banner.png'
 const render = (Container) => {
@@ -25,8 +69,8 @@ const render = (Container) => {
             <Container
                 banner={banner}
                 logo={logo}
-                status={status}
-                friends={friends}
+                status={user.feed.status}
+                friends={user.friends}
                 {...window.INITIAL_STATE}
             />
         </AppContainer>,
