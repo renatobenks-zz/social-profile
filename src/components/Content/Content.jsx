65e95e2646aec988c2df16119a04b84f66a4ee39
Renato Benkendorf <renato-benkendorf@hotmail.com>
2017-04-21 01:06:14 -0300

    working with columns grid in the children components in content component

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import { Grid } from 'semantic-ui-react'
 
 class Content extends Component {
     render () {
         const { children } = this.props;
         return (
-            <div className='App-content'>
-                { children }
+            <div className="App-content">
+                <Grid>
+                    <Grid.Row>
+                        {!Array.isArray(children) ? children : children
+                            .map(content => {
+                                if (children.length <= 3) {
+                                    const index = children
+                                        .indexOf(content);
+                                    const width = index % 2 === 0 ? 4 : 8;
+                                    return (
+                                        <Grid.Column
+                                            key={content.key}
+                                            width={width}
+                                            >
+                                            {content}
+                                        </Grid.Column>
+                                    )
+                                } else {
+                                    return ''
+                                }
+                            })
+                        }
+                    </Grid.Row>
+                </Grid>
             </div>
         )
     }
--- src/components/Content/Content.test.js
+++ src/components/Content/Content.test.js
@@ -1,18 +1,40 @@
-import React from 'react';
-import ReactRender from 'react-test-renderer';
+import React from 'react'
+import ReactRender from 'react-test-renderer'
 
-import Content from './Content.jsx';
-const ContentComponent = ReactRender.create(
+import Content from './Content.jsx'
+
+const content = [
+    <img key="logo" src="logo.svg" alt="logo" />,
+    <p key="text">To get started, edit and save to reload.</p>,
+    <code>Obs.: edit src/App.js</code>
+];
+
+const createComponent = content => ReactRender.create(
     <Content>
-        <p>
-            To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {content}
     </Content>
 );
+
+const ContentComponent = createComponent(content);
 const component = ContentComponent.toJSON();
 
 describe('Component: Content', () => {
     test('renders without crash', () => {
+        expect(component).toMatchSnapshot();
+    });
+
+    test('renders content with two content children', () => {
+        const component = createComponent([...content].splice(0,2));
+        expect(component).toMatchSnapshot();
+    });
+
+    test('renders content with only one content children', () => {
+        const component = createComponent([...content].pop()[0]);
+        expect(component).toMatchSnapshot();
+    });
+
+    test('no renders content when content children is bigger than 3', () => {
+        const component = createComponent([...content, <div>Anything says hello</div>]);
         expect(component).toMatchSnapshot();
     });
 });
