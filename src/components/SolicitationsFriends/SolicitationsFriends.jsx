1b15f28ab77f7c38a4cb1babe59feecbdbe660bd
Renato Benkendorf <renato-benkendorf@hotmail.com>
2017-04-27 00:14:29 -0300

    use data from friends that request friendshipt with user

--- src/components/SolicitationsFriends/SolicitationsFriends.jsx
+++ src/components/SolicitationsFriends/SolicitationsFriends.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Card, Button, Image } from 'semantic-ui-react'
+import { Card, Button, Image, Icon } from 'semantic-ui-react'
 
 class SolicitationsFriends extends Component {
     constructor (props) {
@@ -13,28 +13,35 @@ class SolicitationsFriends extends Component {
         const { friends } = this.state;
         return (
             <Card.Group>
-                {friends.map(friend => (
-                    <Card key={friend.id}>
-                        <Card.Content>
-                            <Image floated='right' size='mini' src={friend.image} />
-                            <Card.Header>
-                                {friend.user ? friend.user : 'Desconhecido'}
-                            </Card.Header>
-                            <Card.Meta>
-                                Friends of Elliot
-                            </Card.Meta>
-                            <Card.Description>
-                                Steve wants to add you to the group <strong>best friends</strong>
-                            </Card.Description>
-                        </Card.Content>
-                        <Card.Content extra>
-                            <div className='ui two buttons'>
-                                <Button basic color='green'>Approve</Button>
-                                <Button basic color='red'>Decline</Button>
-                            </div>
-                        </Card.Content>
-                    </Card>
-                ))}
+                {friends.map(friend => {
+                    friend.user = friend.user ? friend.user : 'Desconhecido';
+                    return (
+                        <Card style={{textAlign: 'left'}} key={friend.id}>
+                            <Card.Content>
+                                <Image
+                                    floated="right"
+                                    size="mini"
+                                    src={friend.image}
+                                />
+                                <Card.Header>
+                                    {friend.user}
+                                </Card.Header>
+                                <Card.Meta>
+                                    Add <Icon link name="add"/>
+                                </Card.Meta>
+                                <Card.Description>
+                                    {friend.user} wants to add you
+                                </Card.Description>
+                            </Card.Content>
+                            <Card.Content extra>
+                                <Button.Group fluid>
+                                    <Button basic color='green'>Approve</Button>
+                                    <Button basic color='red'>Decline</Button>
+                                </Button.Group>
+                            </Card.Content>
+                        </Card>
+                    )
+                })}
             </Card.Group>
         );
     }
