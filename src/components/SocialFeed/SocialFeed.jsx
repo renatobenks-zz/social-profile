1549206b12f6bedd5a49fe556e91863988f6228e
Renato Benkendorf <renato-benkendorf@hotmail.com>
2017-05-01 12:39:40 -0300

    add new message to info the end content for enable come back to the top content into feed of friends status

--- src/components/SocialFeed/SocialFeed.jsx
+++ src/components/SocialFeed/SocialFeed.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Button, Icon } from 'semantic-ui-react'
 
 import BreadcrumbRoute from '../BreadcrumbRoute/BreadcrumbRoute.jsx'
 import Title from '../Title/Title.jsx'
@@ -11,6 +12,17 @@ class SocialFeed extends Component {
                 <Title title="Status | timeline" icon="browser"/>
                 <hr/>
                 {children}
+                <p>
+                    No more content, <strong>sign to the top</strong>
+                    <Button basic inverted animated="vertical">
+                        <Button.Content visible>
+                            <Icon color="grey" name="angle up" />
+                        </Button.Content>
+                        <Button.Content hidden>
+                            <Icon color="grey" name="double angle up" />
+                        </Button.Content>
+                    </Button>
+                </p>
             </div>
         );
     }
